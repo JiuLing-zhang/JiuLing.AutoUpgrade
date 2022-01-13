78349f6d5cf6bed4476f8e513247b8d250d2bba2
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-01-13 15:50:39 +0800

    1.Initialize project.

---
+++ src/FmMain.cs
@@ -0,0 +1,15 @@
+namespace AutoUpgrade
+{
+    public partial class FmMain : Form
+    {
+        public FmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,17 @@
+namespace AutoUpgrade
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new FmMain());
+        }
+    }
+}
