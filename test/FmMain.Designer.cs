c40ebb51da46b6f8f227de88aa5fd74f78a9e3c0
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-01-18 11:48:25 +0800

    1、新增测试程序

---
+++ test/FmMain.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace AutoUpgrade.Test
+{
+    partial class FmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 391);
+            this.Name = "FmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ test/FmMain.cs
@@ -0,0 +1,15 @@
+namespace AutoUpgrade.Test
+{
+    public partial class FmMain : Form
+    {
+        public FmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
