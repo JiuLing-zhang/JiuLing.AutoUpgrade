2a89a47ffd86aee41941f54f0979c9796d211bd9
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-01-21 10:23:47 +0800

    1、项目重命名

--- src/Properties/Resources.Designer.cs
+++ src/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AutoUpgrade.Properties {
+namespace JiuLing.AutoUpgrade.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace AutoUpgrade.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AutoUpgrade.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("JiuLing.AutoUpgrade.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
