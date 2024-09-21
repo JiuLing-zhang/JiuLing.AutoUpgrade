f4628a3eb53a2c48f9231b251df78501cb6e1eb9
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2024-09-21 18:03:35 +0800

    完善测试程序

--- src/JiuLing.AutoUpgrade.Test/FmMain.Designer.cs
+++ src/JiuLing.AutoUpgrade.Test/FmMain.Designer.cs
@@ -42,31 +42,49 @@
             label5 = new Label();
             BtnCheckUpgradeAsync = new Button();
             BtnCheckUpgradeUsingFtpAsync = new Button();
+            groupBoxSetting = new GroupBox();
+            comboBoxVersionFormat = new ComboBox();
+            label10 = new Label();
+            comboBoxIcon = new ComboBox();
+            label9 = new Label();
+            comboBoxLang = new ComboBox();
+            label8 = new Label();
+            numericUpDownTimeoutSecond = new NumericUpDown();
+            comboBoxTheme = new ComboBox();
+            label7 = new Label();
+            label6 = new Label();
+            checkBoxSignCheck = new CheckBox();
+            checkBoxDefaultConfig = new CheckBox();
+            groupBoxSetting.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownTimeoutSecond).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(15, 11);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(56, 17);
+            label1.Size = new Size(69, 20);
             label1.TabIndex = 0;
             label1.Text = "当前版本";
             // 
             // LblVersion
             // 
             LblVersion.AutoSize = true;
-            LblVersion.Location = new Point(74, 9);
+            LblVersion.Location = new Point(95, 11);
+            LblVersion.Margin = new Padding(4, 0, 4, 0);
             LblVersion.Name = "LblVersion";
-            LblVersion.Size = new Size(13, 17);
+            LblVersion.Size = new Size(15, 20);
             LblVersion.TabIndex = 1;
             LblVersion.Text = "-";
             // 
             // BtnCheckUpgrade
             // 
-            BtnCheckUpgrade.Location = new Point(12, 81);
+            BtnCheckUpgrade.Location = new Point(15, 344);
+            BtnCheckUpgrade.Margin = new Padding(4);
             BtnCheckUpgrade.Name = "BtnCheckUpgrade";
-            BtnCheckUpgrade.Size = new Size(149, 48);
+            BtnCheckUpgrade.Size = new Size(192, 56);
             BtnCheckUpgrade.TabIndex = 2;
             BtnCheckUpgrade.Text = "检查更新-HTTP";
             BtnCheckUpgrade.UseVisualStyleBackColor = true;
@@ -74,9 +92,10 @@
             // 
             // BtnCheckUpgradeUsingFtp
             // 
-            BtnCheckUpgradeUsingFtp.Location = new Point(12, 236);
+            BtnCheckUpgradeUsingFtp.Location = new Point(15, 520);
+            BtnCheckUpgradeUsingFtp.Margin = new Padding(4);
             BtnCheckUpgradeUsingFtp.Name = "BtnCheckUpgradeUsingFtp";
-            BtnCheckUpgradeUsingFtp.Size = new Size(149, 48);
+            BtnCheckUpgradeUsingFtp.Size = new Size(192, 56);
             BtnCheckUpgradeUsingFtp.TabIndex = 3;
             BtnCheckUpgradeUsingFtp.Text = "检查更新-FTP";
             BtnCheckUpgradeUsingFtp.UseVisualStyleBackColor = true;
@@ -85,76 +104,85 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 48);
+            label2.Location = new Point(15, 316);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(56, 17);
+            label2.Size = new Size(69, 20);
             label2.TabIndex = 4;
             label2.Text = "更新地址";
             // 
             // txtUpgradeUrl
             // 
             txtUpgradeUrl.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            txtUpgradeUrl.Location = new Point(74, 45);
+            txtUpgradeUrl.Location = new Point(95, 313);
+            txtUpgradeUrl.Margin = new Padding(4);
             txtUpgradeUrl.Name = "txtUpgradeUrl";
-            txtUpgradeUrl.Size = new Size(613, 23);
+            txtUpgradeUrl.Size = new Size(719, 27);
             txtUpgradeUrl.TabIndex = 5;
             txtUpgradeUrl.Text = "https://raw.githubusercontent.com/JiuLing-zhang/AutoUpgrade/main/test/AppInfo.json";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(12, 146);
+            label3.Location = new Point(15, 414);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(56, 17);
+            label3.Size = new Size(69, 20);
             label3.TabIndex = 6;
             label3.Text = "更新地址";
             // 
             // TxtFtpUpgradePath
             // 
             TxtFtpUpgradePath.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            TxtFtpUpgradePath.Location = new Point(74, 143);
+            TxtFtpUpgradePath.Location = new Point(95, 410);
+            TxtFtpUpgradePath.Margin = new Padding(4);
             TxtFtpUpgradePath.Name = "TxtFtpUpgradePath";
-            TxtFtpUpgradePath.Size = new Size(613, 23);
+            TxtFtpUpgradePath.Size = new Size(719, 27);
             TxtFtpUpgradePath.TabIndex = 7;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(24, 176);
+            label4.Location = new Point(31, 449);
+            label4.Margin = new Padding(4, 0, 4, 0);
             label4.Name = "label4";
-            label4.Size = new Size(44, 17);
+            label4.Size = new Size(54, 20);
             label4.TabIndex = 8;
             label4.Text = "用户名";
             // 
             // TxtUserName
             // 
-            TxtUserName.Location = new Point(74, 173);
+            TxtUserName.Location = new Point(95, 446);
+            TxtUserName.Margin = new Padding(4);
             TxtUserName.Name = "TxtUserName";
-            TxtUserName.Size = new Size(185, 23);
+            TxtUserName.Size = new Size(237, 27);
             TxtUserName.TabIndex = 9;
             // 
             // TxtPassword
             // 
-            TxtPassword.Location = new Point(74, 207);
+            TxtPassword.Location = new Point(95, 486);
+            TxtPassword.Margin = new Padding(4);
             TxtPassword.Name = "TxtPassword";
             TxtPassword.PasswordChar = '♥';
-            TxtPassword.Size = new Size(185, 23);
+            TxtPassword.Size = new Size(237, 27);
             TxtPassword.TabIndex = 11;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(36, 210);
+            label5.Location = new Point(46, 489);
+            label5.Margin = new Padding(4, 0, 4, 0);
             label5.Name = "label5";
-            label5.Size = new Size(32, 17);
+            label5.Size = new Size(39, 20);
             label5.TabIndex = 10;
             label5.Text = "密码";
             // 
             // BtnCheckUpgradeAsync
             // 
-            BtnCheckUpgradeAsync.Location = new Point(167, 81);
+            BtnCheckUpgradeAsync.Location = new Point(215, 344);
+            BtnCheckUpgradeAsync.Margin = new Padding(4);
             BtnCheckUpgradeAsync.Name = "BtnCheckUpgradeAsync";
-            BtnCheckUpgradeAsync.Size = new Size(149, 48);
+            BtnCheckUpgradeAsync.Size = new Size(192, 56);
             BtnCheckUpgradeAsync.TabIndex = 12;
             BtnCheckUpgradeAsync.Text = "检查更新-异步-HTTP";
             BtnCheckUpgradeAsync.UseVisualStyleBackColor = true;
@@ -162,19 +190,156 @@
             // 
             // BtnCheckUpgradeUsingFtpAsync
             // 
-            BtnCheckUpgradeUsingFtpAsync.Location = new Point(167, 236);
+            BtnCheckUpgradeUsingFtpAsync.Location = new Point(215, 520);
+            BtnCheckUpgradeUsingFtpAsync.Margin = new Padding(4);
             BtnCheckUpgradeUsingFtpAsync.Name = "BtnCheckUpgradeUsingFtpAsync";
-            BtnCheckUpgradeUsingFtpAsync.Size = new Size(149, 48);
+            BtnCheckUpgradeUsingFtpAsync.Size = new Size(192, 56);
             BtnCheckUpgradeUsingFtpAsync.TabIndex = 13;
             BtnCheckUpgradeUsingFtpAsync.Text = "检查更新-异步-FTP";
             BtnCheckUpgradeUsingFtpAsync.UseVisualStyleBackColor = true;
             BtnCheckUpgradeUsingFtpAsync.Click += BtnCheckUpgradeUsingFtpAsync_Click;
             // 
+            // groupBoxSetting
+            // 
+            groupBoxSetting.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            groupBoxSetting.Controls.Add(comboBoxVersionFormat);
+            groupBoxSetting.Controls.Add(label10);
+            groupBoxSetting.Controls.Add(comboBoxIcon);
+            groupBoxSetting.Controls.Add(label9);
+            groupBoxSetting.Controls.Add(comboBoxLang);
+            groupBoxSetting.Controls.Add(label8);
+            groupBoxSetting.Controls.Add(numericUpDownTimeoutSecond);
+            groupBoxSetting.Controls.Add(comboBoxTheme);
+            groupBoxSetting.Controls.Add(label7);
+            groupBoxSetting.Controls.Add(label6);
+            groupBoxSetting.Controls.Add(checkBoxSignCheck);
+            groupBoxSetting.Location = new Point(15, 66);
+            groupBoxSetting.Name = "groupBoxSetting";
+            groupBoxSetting.Size = new Size(691, 232);
+            groupBoxSetting.TabIndex = 14;
+            groupBoxSetting.TabStop = false;
+            groupBoxSetting.Text = "更新选项";
+            // 
+            // comboBoxVersionFormat
+            // 
+            comboBoxVersionFormat.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxVersionFormat.FormattingEnabled = true;
+            comboBoxVersionFormat.Items.AddRange(new object[] { "", "Major", "MajorMinor", "MajorMinorBuild", "MajorMinorBuildRevision" });
+            comboBoxVersionFormat.Location = new Point(195, 192);
+            comboBoxVersionFormat.Name = "comboBoxVersionFormat";
+            comboBoxVersionFormat.Size = new Size(227, 28);
+            comboBoxVersionFormat.TabIndex = 8;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(90, 200);
+            label10.Name = "label10";
+            label10.Size = new Size(99, 20);
+            label10.TabIndex = 7;
+            label10.Text = "版本展示格式";
+            // 
+            // comboBoxIcon
+            // 
+            comboBoxIcon.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxIcon.FormattingEnabled = true;
+            comboBoxIcon.Items.AddRange(new object[] { "", "icon_test_1.ico", "icon_test_2.ico" });
+            comboBoxIcon.Location = new Point(195, 158);
+            comboBoxIcon.Name = "comboBoxIcon";
+            comboBoxIcon.Size = new Size(227, 28);
+            comboBoxIcon.TabIndex = 8;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(150, 166);
+            label9.Name = "label9";
+            label9.Size = new Size(39, 20);
+            label9.TabIndex = 7;
+            label9.Text = "图标";
+            // 
+            // comboBoxLang
+            // 
+            comboBoxLang.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxLang.FormattingEnabled = true;
+            comboBoxLang.Items.AddRange(new object[] { "", "zh", "en" });
+            comboBoxLang.Location = new Point(195, 124);
+            comboBoxLang.Name = "comboBoxLang";
+            comboBoxLang.Size = new Size(227, 28);
+            comboBoxLang.TabIndex = 8;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(150, 132);
+            label8.Name = "label8";
+            label8.Size = new Size(39, 20);
+            label8.TabIndex = 7;
+            label8.Text = "语言";
+            // 
+            // numericUpDownTimeoutSecond
+            // 
+            numericUpDownTimeoutSecond.Location = new Point(195, 53);
+            numericUpDownTimeoutSecond.Name = "numericUpDownTimeoutSecond";
+            numericUpDownTimeoutSecond.Size = new Size(125, 27);
+            numericUpDownTimeoutSecond.TabIndex = 6;
+            numericUpDownTimeoutSecond.Value = new decimal(new int[] { 5, 0, 0, 0 });
+            // 
+            // comboBoxTheme
+            // 
+            comboBoxTheme.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxTheme.FormattingEnabled = true;
+            comboBoxTheme.Items.AddRange(new object[] { "", "System", "Light", "Dark" });
+            comboBoxTheme.Location = new Point(195, 90);
+            comboBoxTheme.Name = "comboBoxTheme";
+            comboBoxTheme.Size = new Size(227, 28);
+            comboBoxTheme.TabIndex = 5;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(150, 98);
+            label7.Name = "label7";
+            label7.Size = new Size(39, 20);
+            label7.TabIndex = 4;
+            label7.Text = "主题";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(15, 56);
+            label6.Name = "label6";
+            label6.Size = new Size(174, 20);
+            label6.TabIndex = 2;
+            label6.Text = "检查更新超时时间（秒）";
+            // 
+            // checkBoxSignCheck
+            // 
+            checkBoxSignCheck.AutoSize = true;
+            checkBoxSignCheck.Location = new Point(195, 23);
+            checkBoxSignCheck.Name = "checkBoxSignCheck";
+            checkBoxSignCheck.Size = new Size(121, 24);
+            checkBoxSignCheck.TabIndex = 1;
+            checkBoxSignCheck.Text = "文件签名校验";
+            checkBoxSignCheck.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDefaultConfig
+            // 
+            checkBoxDefaultConfig.AutoSize = true;
+            checkBoxDefaultConfig.Location = new Point(15, 37);
+            checkBoxDefaultConfig.Name = "checkBoxDefaultConfig";
+            checkBoxDefaultConfig.Size = new Size(121, 24);
+            checkBoxDefaultConfig.TabIndex = 0;
+            checkBoxDefaultConfig.Text = "使用默认设置";
+            checkBoxDefaultConfig.UseVisualStyleBackColor = true;
+            checkBoxDefaultConfig.CheckedChanged += checkBoxDefaultConfig_CheckedChanged;
+            // 
             // FmMain
             // 
-            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(701, 296);
+            ClientSize = new Size(833, 583);
+            Controls.Add(groupBoxSetting);
             Controls.Add(BtnCheckUpgradeUsingFtpAsync);
             Controls.Add(BtnCheckUpgradeAsync);
             Controls.Add(TxtPassword);
@@ -185,16 +350,21 @@
             Controls.Add(label3);
             Controls.Add(txtUpgradeUrl);
             Controls.Add(label2);
+            Controls.Add(checkBoxDefaultConfig);
             Controls.Add(BtnCheckUpgradeUsingFtp);
             Controls.Add(BtnCheckUpgrade);
             Controls.Add(LblVersion);
             Controls.Add(label1);
+            Margin = new Padding(4);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "FmMain";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "自动更新测试程序";
             Load += FmMain_Load;
+            groupBoxSetting.ResumeLayout(false);
+            groupBoxSetting.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownTimeoutSecond).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -215,5 +385,18 @@
         private Label label5;
         private Button BtnCheckUpgradeAsync;
         private Button BtnCheckUpgradeUsingFtpAsync;
+        private GroupBox groupBoxSetting;
+        private CheckBox checkBoxDefaultConfig;
+        private CheckBox checkBoxSignCheck;
+        private Label label6;
+        private Label label7;
+        private ComboBox comboBoxTheme;
+        private NumericUpDown numericUpDownTimeoutSecond;
+        private ComboBox comboBoxLang;
+        private Label label8;
+        private ComboBox comboBoxIcon;
+        private Label label9;
+        private ComboBox comboBoxVersionFormat;
+        private Label label10;
     }
 }
--- src/JiuLing.AutoUpgrade.Test/FmMain.cs
+++ src/JiuLing.AutoUpgrade.Test/FmMain.cs
@@ -1,81 +1,132 @@
-using JiuLing.AutoUpgrade.Shell;
+using JiuLing.AutoUpgrade.Shared;
+using JiuLing.AutoUpgrade.Shell.Creator;
+using JiuLing.AutoUpgrade.Shell.Enums;
 
-namespace JiuLing.AutoUpgrade.Test
+namespace JiuLing.AutoUpgrade.Test;
+public partial class FmMain : Form
 {
-    public partial class FmMain : Form
+    private IUpgradeApp _upgradeApp;
+    public FmMain()
     {
-        public FmMain()
+        InitializeComponent();
+    }
+
+    private void FmMain_Load(object sender, EventArgs e)
+    {
+        LblVersion.Text = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString();
+        comboBoxTheme.SelectedIndex = 0;
+        comboBoxLang.SelectedIndex = 0;
+        comboBoxIcon.SelectedIndex = 0;
+        comboBoxVersionFormat.SelectedIndex = 0;
+        checkBoxDefaultConfig.Checked = true;
+    }
+
+    private void BtnCheckUpgrade_Click(object sender, EventArgs e)
+    {
+        try
         {
-            InitializeComponent();
+            BuildUpgradeHttpApp().Run();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"操作失败：{ex.Message}");
+        }
+    }
+
+    private async void BtnCheckUpgradeAsync_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            await BuildUpgradeHttpApp().RunAsync();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"操作失败：{ex.Message}");
+        }
+    }
+
+    private void BtnCheckUpgradeUsingFtp_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            BuildUpgradeFtpApp().Run();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"操作失败：{ex.Message}");
+        }
+    }
+
+    private async void BtnCheckUpgradeUsingFtpAsync_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            await BuildUpgradeFtpApp().RunAsync();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"操作失败：{ex.Message}");
+        }
+    }
+
+    private void checkBoxDefaultConfig_CheckedChanged(object sender, EventArgs e)
+    {
+        var enabled = !checkBoxDefaultConfig.Checked;
+        foreach (Control control in groupBoxSetting.Controls)
+        {
+            control.Enabled = enabled;
+        }
+    }
+
+    private IUpgradeApp BuildUpgradeHttpApp()
+    {
+        _upgradeApp = UpgradeFactory.CreateHttpApp(txtUpgradeUrl.Text);
+        if (!checkBoxDefaultConfig.Checked)
+        {
+            _upgradeApp.SetUpgrade(BuildUpgradeSetting());
+        }
+        return _upgradeApp;
+    }
+
+    private IUpgradeApp BuildUpgradeFtpApp()
+    {
+        _upgradeApp = UpgradeFactory.CreateFtpApp(TxtFtpUpgradePath.Text, TxtUserName.Text, TxtPassword.Text);
+        if (!checkBoxDefaultConfig.Checked)
+        {
+            _upgradeApp.SetUpgrade(BuildUpgradeSetting());
+        }
+        return _upgradeApp;
+    }
+
+    private UpgradeSettingBuilder BuildUpgradeSetting()
+    {
+        var setting = new UpgradeSettingBuilder();
+        setting.WithSignCheck(checkBoxSignCheck.Checked);
+        setting.WithTimeout(Convert.ToInt32(numericUpDownTimeoutSecond.Value));
+
+        var theme = comboBoxTheme.Text;
+        if (!string.IsNullOrEmpty(theme))
+        {
+            setting.WithTheme((ThemeEnum)Enum.Parse(typeof(ThemeEnum), theme));
         }
 
-        private void FmMain_Load(object sender, EventArgs e)
+        var lang = comboBoxLang.Text;
+        if (!string.IsNullOrEmpty(lang))
         {
-            LblVersion.Text = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString();
+            setting.WithLang(lang);
         }
 
-        private void BtnCheckUpgrade_Click(object sender, EventArgs e)
+        var icon = comboBoxIcon.Text;
+        if (!string.IsNullOrEmpty(icon))
         {
-            try
-            {
-                var app = AutoUpgradeFactory.Create();
-                app.SetUpgrade(config =>
-                {
-                    config.IsCheckSign = true;
-                });
-                app.UseHttpMode(txtUpgradeUrl.Text)
-                     .Run();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"操作失败：{ex.Message}");
-            }
+            setting.WithIcon(icon);
         }
 
-        private async void BtnCheckUpgradeAsync_Click(object sender, EventArgs e)
+        var versionFormat = comboBoxVersionFormat.Text;
+        if (!string.IsNullOrEmpty(versionFormat))
         {
-            try
-            {
-                var app = AutoUpgradeFactory.Create();
-                app.SetUpgrade(config =>
-                {
-                    config.IsCheckSign = true;
-                });
-                await app.UseHttpMode(txtUpgradeUrl.Text)
-                      .RunAsync();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"操作失败：{ex.Message}");
-            }
+            setting.WithVersionFormat((VersionFormatEnum)Enum.Parse(typeof(VersionFormatEnum), versionFormat));
         }
-
-        private void BtnCheckUpgradeUsingFtp_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                var app = AutoUpgradeFactory.Create();
-                app.UseFtpMode(TxtFtpUpgradePath.Text, TxtUserName.Text, TxtPassword.Text)
-                    .Run();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"操作失败：{ex.Message}");
-            }
-        }
-
-        private async void BtnCheckUpgradeUsingFtpAsync_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                var app = AutoUpgradeFactory.Create();
-                await app.UseFtpMode(TxtFtpUpgradePath.Text, TxtUserName.Text, TxtPassword.Text)
-                      .RunAsync();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"操作失败：{ex.Message}");
-            }
-        }
+        return setting;
     }
 }
