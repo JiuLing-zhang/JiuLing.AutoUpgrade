82f631fd41755fa84f0c242d608dee77aef66d4b
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2024-09-22 17:01:24 +0800

    删除历史接口

--- src/JiuLing.AutoUpgrade.Shell/Strategies/NetworkStrategy.cs
+++
@@ -1,9 +0,0 @@
-﻿using JiuLing.AutoUpgrade.Shell.Enums;
-
-namespace JiuLing.AutoUpgrade.Shell.Strategies
-{
-    internal abstract class NetworkStrategy
-    {
-        public abstract string Build(NetworkTypeEnum networkType);
-    }
-}
